f7364ba7ffa9b1553bb67fca3ce71ff0d5b817be
Sergey Eroshenkov <itechmeat@gmail.com>
2024-11-09 12:37:34 +0100

    Initial design

---
+++ src/i18n/locales/en.ts
@@ -0,0 +1,11 @@
+const translation = {
+  HomePage: {
+    title: "Let's write a new story!",
+    cta: "Start writing",
+  },
+  header: {
+    projects: "Stories",
+  },
+};
+
+export default translation;
---
+++ src/i18n/locales/ru.ts
@@ -0,0 +1,11 @@
+const translation = {
+  HomePage: {
+    title: "Давайте напишем новую историю!",
+    cta: "Начать писать",
+  },
+  header: {
+    projects: "Истории",
+  },
+};
+
+export default translation;
